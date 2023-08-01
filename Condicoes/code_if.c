b0c26237bceb016cf3ecd13f42ca17fc9ce16e42
mayress <mfss2@cin.ufpe.br>
2023-08-01 05:01:51 -0300

    Testando coisas de tabuada

--- Condicoes/code_if.c
+++ Condicoes/code_if.c
@@ -5,37 +5,43 @@ int main () {
     int n=0, m=0;
     float resul=0;
 
+    printf("Escolha um numero qualquer: ");
     scanf("%d", &n);
 
-    printf("O que você prefere?\n\t\t1. soma\n\t\t2. subtracao\n\t\t3. multiplicacao\n\t\t4. divisao\n");
+    printf("O que você prefere?\n\t1. soma\n\t2. subtracao\n\t3. multiplicacao\n\t4. divisao\n");
     scanf("%d", &escolha);
     
     if(escolha>=1 && escolha<=4){
+        printf("Escolha outro numero qualquer: ");
         scanf("%d", &m);
-    }
-
-    if(escolha == 1){
-        resul = n + m;
-    }
-    else if(escolha == 2){
-        resul = n - m;
-    }
-    else if(escolha == 3){
-        resul = n * m;
-    }
-    else if(escolha == 4){
+        //calculando
+        if(escolha == 1){
+            resul = n + m;
+        }
+        else if(escolha == 2){
+            resul = n - m;
+        }
+        else if(escolha == 3){
+            resul = n * m;
+        }
+        else if(escolha == 4){
+            resul = n / m;
+        }
+        //printando
         if(n!=0 || m!=0){
-            if(n!=0) resul = m / n;
-            else if(m!=0) resul = n / m;
+            printf("O resultado final eh %.0f\n", resul);
+        }
+        else if(n==0 && m==0){
+            printf("O resultado final eh infinito\n");
+        }
+        else {
+            printf("Nao sei\n");
         }
     }
     else {
         printf("Opcao nao eh valida\nTente de novo\n");
     }
 
-    if(escolha>=1 && escolha<=4){
-        printf("O resultado final é %i\n", resul);
-    }
 
     return 0;
 }
