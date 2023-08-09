6e0aa22f4ccdd65842f94460f334fe646003115c
mayress <mfss2@cin.ufpe.br>
2023-08-09 15:45:01 -0300

    update da lista6

--- Ponteiros/listas/lista_6.c
+++ Ponteiros/listas/lista_6.c
@@ -1,13 +1,12 @@
-/*
-1. Declare uma variável do tipo char chamada c e um ponteiro para char chamado pc. Inicialize
+
+/*1. Declare uma variável do tipo char chamada c e um ponteiro para char chamado pc. Inicialize
 c com o valor ‘a’ e pc com o endereço de c. Utilizando %p para endereços de memória, Imprima as
 seguintes linhas
 */
-
+/*
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 
 int main(){
 	char c;
@@ -33,14 +32,17 @@ int main(){
     printf("o endereco de pc: %p\n", &pc);
     //&pc nao mostra exatamente o endereço de c
     printf("o endereco de pc: %p\n", *pc);
+    //*pc nao mostra exatamente o endereço de c
 
     //endereço do valor guardado no endereço apontado por pc
     printf("\nb)\n"); 
     printf("Endereco do valor guardado no endereco apontado por pc: %p\n", &*pc);
     printf("Endereco do ponteiro: %p\n", *&pc);
 
+	return 0;
+}
+*/
     /*
-
     explicação: 
         o '&' traz o endereco de determinada variavel,
         um ponteiro é um endereco de memoria, 
@@ -49,45 +51,99 @@ int main(){
         e nele esta "guardado", apontando, o endereco de outra variavel, 
         e para mostrar o endereco apontado pelo ponteiro 
         '*' e '&' funcionam do mesmo jeito no print para ponteiro
-    
+        (APENAS QUANDO JUNTOS PARA A MESMA FUNCAO).
     */
 
-
-
-	return 0;
-}
-
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
-    /*3. Usando somente as variáveis ptr_str, ptr_inv e i, complete o código abaixo para que ele que
-inverta a string str e coloque na str_inv. (Não use o operador [ ] nem str e str_inv, utilize
-somente os ponteiros e a aritmética de ponteiros)*/
 
+/*2. Declare um vetor de inteiros vet[5] = {1,2,3,4,5} e um ponteiro de inteiros p que aponte para esse
+vetor. Imprima
+*/
 /*
-
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+int main (){
+    int vet[5] = {1,2,3,4,5};
+    int *p = vet;
 
-int main( ){
-    char str[50]; // String
-    char str_inv[50]; // String invertida
-    char *ptr_str = str;
-    char *ptr_inv = str_inv;
-    int i=-1;
-    scanf(" %s", str);
-     //continue o código a partir daqui
+    printf("a)\n");
+    printf("Endereco guardado em vet: %p\n", &vet);
+    printf("Endereco guardado em p: %p\n", &*p);
 
-    printf(" O inverso da string : %s\n\n",str_inv);
+    printf("\nb)\n");
+    for(int i=0; i<5; i++){
+        printf("Valor (p) vet[%d]: %d\n", i+1, p[i]);
+    }
+
+    printf("\nc)\n");
+    for(int i=0; i<5; i++){
+        printf("Valor de (p)vet[%d]: %d\n", i+1, *(p+i));
+    }
+
+    printf("\nd)\n");
+    for(int i=0; i<5; i++){
+        printf("Valor de vet[%d]: %d\n", i+1, vet[i]);
+    }
+    printf("\n");
+    for(int i=0; i<5; i++){
+        printf("Valor de vet[%d]: %d\n", i+1, *(vet+i));
+    }
 
     return 0;
 }
-
 */
 
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
 
-    /*4. Faça um programa que aloque dinamicamente um vetor de strings str, receba de entrada várias
-strings enquanto existir entrada no buffer e guarde-as nesse vetor utilizando ponteiros auxiliares 
-(ponteiro temporário na hora de dar realloc).*/
+/*3. Usando somente as variáveis ptr_str, ptr_inv e i, complete o código abaixo para que ele que
+inverta a string str e coloque na str_inv. (Não use o operador [ ] nem str e str_inv, utilize
+somente os ponteiros e a aritmética de ponteiros)*/
+/*
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[50]; // String
+    char str_inv[50]; // String invertida
+    char *ptr_str = str;
+    char *ptr_inv = str_inv;
+    int i = -1;
+    scanf(" %s", str);
+
+    //lê a string de apenas uma palavra
+
+    //solução
+    //comprimento da string
+    while (*ptr_str != '\0') {
+        ptr_str++;
+        i++;
+    }
+    //ptr_str já está apontando para o último caractere (o nulo) da string original
+
+    //string sendo invertida
+    while (i >= 0) {
+        ptr_str--;
+        *ptr_inv = *ptr_str;
+        ptr_inv++;
+        i--;
+    }
+    
+    *ptr_inv = '\0'; //caracter nulo no final para printar
+
+    //saída
+    printf(" O inverso da string : %s\n\n", str_inv);
+
+
+    return 0;
+}
+*/
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+    /*4. Faça um programa que aloque dinamicamente um vetor de 
+    strings str, receba de entrada várias strings enquanto 
+    existir entrada no buffer e guarde-as nesse vetor utilizando 
+    ponteiros auxiliares (ponteiro temporário na hora de dar realloc).*/
+
+
     
 
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
